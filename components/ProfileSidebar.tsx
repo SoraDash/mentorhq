6c30e2e0d523a385a9ca294789575950650d7d09
Simen Daehlin <simen.daehlin@strapi.io>
2023-09-13 14:33:33 +0100

    Fixed wrong photo from github

--- components/ProfileSidebar.tsx
+++ components/ProfileSidebar.tsx
@@ -43,53 +43,53 @@ export const ProfileSidebar: React.FC<ProfileSidebarProps> = ({ profile }) => {
     <div className="dark:bg-navbar bg-white shadow rounded-lg p-6">
       <div className="flex flex-col items-center space-y-5">
         <Avatar
-          isBordered={profile.isPremium}
-          color={profile.isPremium ? "danger" : "default"}
+          isBordered={ profile.isPremium }
+          color={ profile.isPremium ? "danger" : "default" }
           className="transition-transform w-32 h-32"
-          src={profile.github as string || profile.image as string}
-          name={profile?.name as string}
+          src={ `https://github.com/${profile.github}.png` || profile.image as string }
+          name={ profile?.name as string }
           showFallback
         />
 
-        <h1 className="text-xl font-bold">{profile.name}</h1>
-        {isMentor(profile) && profile.role && (
+        <h1 className="text-xl font-bold">{ profile.name }</h1>
+        { isMentor(profile) && profile.role && (
           <div className="text-gray-600 flex flex-col items-center space-y-3">
             <div className="flex items-center font-bold">
-              {capitalize(profile.role)}
-              <span className={cn("w-5 h-5 ml-1", color ? color : 'text-gray-600')}>
+              { capitalize(profile.role) }
+              <span className={ cn("w-5 h-5 ml-1", color ? color : 'text-gray-600') }>
                 <RoleIcon />
               </span>
             </div>
-            {profile.isPremium && (
-              <Badge variant={"premium"}>
+            { profile.isPremium && (
+              <Badge variant={ "premium" }>
                 Premium Member
               </Badge>
-            )}
+            ) }
           </div>
-        )}
+        ) }
 
         <div className="mt-6 flex flex-wrap gap-4 justify-center">
           <div className="mt-6 grid grid-cols-1 gap-3">
 
-            <IconButton color="bg-secondary text-secondary-foreground" icon={FaUserEdit}>
+            <IconButton color="bg-secondary text-secondary-foreground" icon={ FaUserEdit }>
               Edit
             </IconButton>
-            {isMentor(profile) && (
+            { isMentor(profile) && (
               <>
-                {profile.github && <SyncGithubBio id={profile.id} />}
+                { profile.github && <SyncGithubBio id={ profile.id } /> }
                 <CalendlyAuth />
               </>
-            )}
-            {isMentor(profile) && profile.role === 'ADMIN' && (
-              <IconButton color="bg-red-500 text-white" icon={FaHammer}>
+            ) }
+            { isMentor(profile) && profile.role === 'ADMIN' && (
+              <IconButton color="bg-red-500 text-white" icon={ FaHammer }>
                 Ban
               </IconButton>
-            )}
+            ) }
           </div>
         </div>
 
 
-        {isMentor(profile) && (
+        { isMentor(profile) && (
           <>
             <hr className="my-6 border-t border-gray-300" />
             <h3 className="text-gray-600 uppercase font-bold tracking-wider mb-2 text-left w-full">Stats</h3>
@@ -98,39 +98,39 @@ export const ProfileSidebar: React.FC<ProfileSidebarProps> = ({ profile }) => {
                 <li className="flex items-center space-x-2">
                   <FaFileAlt className="text-gray-500" />
                   <span className="flex-grow">Sessions</span>
-                  <span>{profile._count.studentSession}</span>
+                  <span>{ profile._count.studentSession }</span>
                 </li>
                 <li className="flex items-center space-x-2">
                   <FaGraduationCap className="text-gray-500" />
                   <span className="flex-grow">Students</span>
-                  <span>{profile._count.students}</span>
+                  <span>{ profile._count.students }</span>
                 </li>
-                {isMentor(profile) && (
+                { isMentor(profile) && (
                   <>
                     <li className="flex items-center space-x-2">
                       <span className="flex-grow">Onboarding Completed</span>
-                      <BooleanIcon condition={profile.isOnboarded} className='items-center text-center w-6 h-6' />
+                      <BooleanIcon condition={ profile.isOnboarded } className='items-center text-center w-6 h-6' />
                     </li>
                     <li className="flex items-center space-x-2">
                       <span className="flex-grow">Premium Member</span>
-                      <BooleanIcon condition={profile.isPremium} className='items-center text-center w-6 h-6' />
+                      <BooleanIcon condition={ profile.isPremium } className='items-center text-center w-6 h-6' />
                     </li>
                     <li className="flex items-center space-x-2">
                       <CalendlyStatus />
                     </li>
                     <li className="flex items-center space-x-2">
                       <span className="flex-grow">CI API Key set</span>
-                      <BooleanIcon condition={!!profile.ciApiKey} className='items-center text-center w-6 h-6' />
+                      <BooleanIcon condition={ !!profile.ciApiKey } className='items-center text-center w-6 h-6' />
                     </li>
                   </>
-                )}
+                ) }
               </ul>
             </div>
           </>
-        )}
+        ) }
         <h3 className="text-gray-600 uppercase font-bold tracking-wider my-3 text-left w-full">Social</h3>
         <div className="flex justify-center items-center gap-6 my-6 w-full">
-          <SocialMediaIcons user={profile} />
+          <SocialMediaIcons user={ profile } />
         </div>
       </div>
     </div>
--- components/client/UserAccountNav.tsx
+++ components/client/UserAccountNav.tsx
@@ -18,31 +18,31 @@ export const UserAccountNav = ({ user, role }: Props) => {
 
 
   return (
-    <Dropdown placement="bottom-end" backdrop='blur' shouldCloseOnInteractOutside={() => true}>
+    <Dropdown placement="bottom-end" backdrop='blur' shouldCloseOnInteractOutside={ () => true }>
       <DropdownTrigger>
         <User
           as="button"
-          avatarProps={{
+          avatarProps={ {
             isBordered: user.isPremium,
-            src: user.github as string || user.image as string,
+            src: `https://github.com/${user.github}.png` || user.image as string,
             showFallback: true,
             color: user.isPremium ? "danger" : "default",
-          }}
+          } }
           className="transition-transform"
-          description={user.email}
-          name={user?.name}
+          description={ user.email }
+          name={ user?.name }
         />
 
       </DropdownTrigger>
       <DropdownMenu aria-label="User Account Navigation" variant="flat">
-        <DropdownItem key="profile" startContent={<FaUserCircle className={iconClasses} />}>My Profile</DropdownItem>
-        <DropdownItem key="settings" startContent={<FaCog className={iconClasses} />}>
+        <DropdownItem key="profile" startContent={ <FaUserCircle className={ iconClasses } /> } onClick={ () => router.push(`/profile`) }  >My Profile</DropdownItem>
+        <DropdownItem key="settings" startContent={ <FaCog className={ iconClasses } /> }>
           My Settings
         </DropdownItem>
-        <DropdownItem key="help_and_feedback" startContent={<FaLifeRing className={iconClasses} />}>
+        <DropdownItem key="help_and_feedback" startContent={ <FaLifeRing className={ iconClasses } /> }>
           Help & Feedback
         </DropdownItem>
-        <DropdownItem key="logout" color="danger" startContent={<FaSignOutAlt className={iconClasses} />} onClick={() => signOut()}>
+        <DropdownItem key="logout" color="danger" startContent={ <FaSignOutAlt className={ iconClasses } /> } onClick={ () => signOut() }>
           Log Out
         </DropdownItem>
       </DropdownMenu>

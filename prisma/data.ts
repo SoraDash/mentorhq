f7d1648e323e6fc263368d95d0b76fddc91bd9ce
Simen Daehlin <simen.daehlin@strapi.io>
2023-09-13 14:33:59 +0100

    Moved seed as it would reseed all the time

---
+++ prisma/data.ts
@@ -0,0 +1,84 @@
+
+export const ppCourses = [
+  { name: "HTML/CSS Essentials (PP1)", prefix: "pp1" },
+  { name: "JavaScript Essentials (PP2)", prefix: "pp2" },
+  { name: "Python Essentials (PP3)", prefix: "pp3" },
+  { name: "Full Stack Toolkit (PP4)", prefix: "pp4" },
+];
+export const msCourses = [
+  { name: "User Centric Front End Development (MS1)", prefix: "ms1" },
+  { name: "Interactive Front End Development (MS2)", prefix: "ms2" },
+  { name: "Data Centric Development (MS3)", prefix: "ms3" },
+  { name: "Full Stack Frameworks with Django (MS4)", prefix: "ms4" },
+];
+
+export const courses = [
+  {
+    courseCode: "diwad",
+    months: 12,
+    projectCount: 4,
+    prefix: "MS",
+    projectStages: [...msCourses],
+    hasSpecialization: false,
+    name: "Diploma In Web Application Development",
+    description: "The old 4 project course",
+  },
+  {
+    courseCode: "disd",
+    months: 12,
+    projectCount: 5,
+    prefix: "PP",
+    projectStages: [...ppCourses],
+    name: "Diploma In Software Development",
+    description: "5 project course up to specialisations (PP1-PP4)",
+  },
+  {
+    courseCode: "disdcc",
+    months: 12,
+    projectCount: 4,
+    prefix: "PP",
+    projectStages: [...ppCourses],
+    name: "Diploma In Software Development Common Curriculum",
+    description: "5 project course up to specialisations (PP1-PP4)",
+  },
+  {
+    courseCode: "disde",
+    months: 12,
+    projectCount: 5,
+    prefix: "PP",
+    projectStages: [...ppCourses],
+    specialization: [{ name: "eCommerce (PP5)", prefix: "pp5" }],
+    name: "Diploma In Software Development E-commerce (Default)",
+    description:
+      "E-commerce specialisation course assigned by default (PP1-PP5)",
+  },
+  {
+    courseCode: "specomm",
+    months: 12,
+    projectCount: 5,
+    prefix: "PP",
+    hasSpecialization: true,
+    specialization: [{ name: "eCommerce (PP5)", prefix: "pp5" }],
+    name: "Diploma In Software Development E-commerce (Chosen)",
+    description:
+      "E-commerce specialisation course chosen by the student (PP1-PP5)",
+  },
+  {
+    courseCode: "sppredan",
+    months: 12,
+    projectCount: 5,
+    prefix: "PP",
+    projectStages: [...ppCourses],
+    name: "Specialization in Predictive Analytics",
+    description: "Predictive Analytics specialisation course (PP1-PP5)",
+  },
+  {
+    courseCode: "spadvfe",
+    months: 12,
+    projectCount: 5,
+    prefix: "PP",
+    projectStages: [...ppCourses],
+    name: "Specialization in Advanced Frontend",
+    description: "Advanced Frontend specialisation course (PP1-PP5)",
+  },
+];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,91 +1,9 @@
+import { courses } from './data';
 
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-const ppCourses = [
-  { name: "HTML/CSS Essentials (PP1)", prefix: "pp1" },
-  { name: "JavaScript Essentials (PP2)", prefix: "pp2" },
-  { name: "Python Essentials (PP3)", prefix: "pp3" },
-  { name: "Full Stack Toolkit (PP4)", prefix: "pp4" },
-];
-const msCourses = [
-  { name: "User Centric Front End Development (MS1)", prefix: "ms1" },
-  { name: "Interactive Front End Development (MS2)", prefix: "ms2" },
-  { name: "Data Centric Development (MS3)", prefix: "ms3" },
-  { name: "Full Stack Frameworks with Django (MS4)", prefix: "ms4" },
-];
-
-const courses = [
-  {
-    courseCode: "diwad",
-    months: 12,
-    projectCount: 4,
-    prefix: "MS",
-    projectStages: [...msCourses],
-    hasSpecialization: false,
-    name: "Diploma In Web Application Development",
-    description: "The old 4 project course",
-  },
-  {
-    courseCode: "disd",
-    months: 12,
-    projectCount: 5,
-    prefix: "PP",
-    projectStages: [...ppCourses],
-    name: "Diploma In Software Development",
-    description: "5 project course up to specialisations (PP1-PP4)",
-  },
-  {
-    courseCode: "disdcc",
-    months: 12,
-    projectCount: 4,
-    prefix: "PP",
-    projectStages: [...ppCourses],
-    name: "Diploma In Software Development Common Curriculum",
-    description: "5 project course up to specialisations (PP1-PP4)",
-  },
-  {
-    courseCode: "disde",
-    months: 12,
-    projectCount: 5,
-    prefix: "PP",
-    projectStages: [...ppCourses],
-    specialization: [{ name: "eCommerce (PP5)", prefix: "pp5" }],
-    name: "Diploma In Software Development E-commerce (Default)",
-    description:
-      "E-commerce specialisation course assigned by default (PP1-PP5)",
-  },
-  {
-    courseCode: "specomm",
-    months: 12,
-    projectCount: 5,
-    prefix: "PP",
-    hasSpecialization: true,
-    specialization: [{ name: "eCommerce (PP5)", prefix: "pp5" }],
-    name: "Diploma In Software Development E-commerce (Chosen)",
-    description:
-      "E-commerce specialisation course chosen by the student (PP1-PP5)",
-  },
-  {
-    courseCode: "sppredan",
-    months: 12,
-    projectCount: 5,
-    prefix: "PP",
-    projectStages: [...ppCourses],
-    name: "Specialization in Predictive Analytics",
-    description: "Predictive Analytics specialisation course (PP1-PP5)",
-  },
-  {
-    courseCode: "spadvfe",
-    months: 12,
-    projectCount: 5,
-    prefix: "PP",
-    projectStages: [...ppCourses],
-    name: "Specialization in Advanced Frontend",
-    description: "Advanced Frontend specialisation course (PP1-PP5)",
-  },
-];
 
 async function main() {
   for (const courseInfo of courses) {
@@ -135,7 +53,7 @@ async function main() {
       });
 
       if (existingProjectTemplate) {
-        console.warn(`⚠️ Project template ${projectTemplateInfo.name} for course ${createdCourse.name} already exists. Skipping...`);
+        console.warn(`⚠️  Project template ${projectTemplateInfo.name} for course ${createdCourse.name} already exists. Skipping...`);
         continue;
       }
 

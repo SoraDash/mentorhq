934e6e8f87e4a72be5fee605df98096ef30c3be3
Simen Daehlin <simen.daehlin@strapi.io>
2023-12-20 17:21:49 +0000

    Add DELETE route for sessions and update PATCH route

--- app/api/sessions/route.ts
+++ app/api/sessions/route.ts
@@ -1,41 +1,21 @@
-import { prisma } from '@/lib/db/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function PATCH(request: NextRequest) {
+  console.log("Received PATCH request:", request); // Debugging log
+
   try {
     const data = await request.json();
-    const { studentId } = data;
 
-    // Check for existing session today
-    const today = new Date().toISOString().split('T')[0];
-    let existingSession = await prisma.project.findFirst({
-      where: {
-        studentId,
-        created_at: today,
-      },
-    });
+    // Mocking a response
+    const mockResponse = {
+      id: "mocked-uuid-1234",
+      ...data,
+    };
 
-    if (existingSession) {
-      // Update existing session
-      const updatedSession = await prisma.studentSession.update({
-        where: { id: existingSession.id },
-        data: {
-          ...data,
-        },
-      });
-      return NextResponse.json(updatedSession, { status: 200 });
-    } else {
-      // Create new session with default data
-      const newSession = await prisma.studentSession.create({
-        data: {
-          studentId: studentId,
-          // Add other default values here
-          ...data,
-        },
-      });
-      return NextResponse.json(newSession, { status: 201 });
-    }
+    console.log("Mock PATCH Response:", mockResponse); // Debugging log
+    return NextResponse.json(mockResponse, { status: data.id ? 200 : 201 }); // 201 for created, 200 for updated
   } catch (error) {
-    return NextResponse.json("Error processing request", { status: 500 });
+    console.error("Error in PATCH route:", error); // Error log
+    return NextResponse.json("Error processing PATCH request", { status: 500 });
   }
 }
--- components/client/sessions/StepFinal.tsx
+++ components/client/sessions/StepFinal.tsx
@@ -1,13 +1,87 @@
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardBody,
+  Button,
+} from "@nextui-org/react";
 import { FormikValues, useFormikContext } from "formik";
+import {
+  CreditCardIcon,
+  CalendarIcon,
+  ClockIcon,
+  FolderIcon,
+  BarChartIcon,
+  LaptopIcon,
+  PlaneIcon,
+  MailboxIcon,
+  FileIcon,
+} from "lucide-react";
 import React from "react";
+import { format } from "date-fns";
 
 interface StepFinalProps {}
 
 export const StepFinal: React.FC<StepFinalProps> = () => {
   const { values } = useFormikContext<FormikValues>();
   return (
-    <div>
-      <pre>{JSON.stringify(values, null, 2)}</pre>
+    <div className="grid grid-cols-2 gap-4">
+      <p className="col-span-2">
+        <CreditCardIcon className="w-4 h-4 inline-block mr-2" />
+        <span className="font-semibold mr-1">Student ID:</span>
+        <span>{values.studentId}</span>
+      </p>
+      <p>
+        <CalendarIcon className="w-4 h-4 inline-block mr-2" />
+        <span className="font-semibold mr-1">Date:</span>
+        <span>{format(new Date(values.date), "dd MMM yyyy")}</span>
+      </p>
+      <p>
+        <ClockIcon className="w-4 h-4 inline-block mr-2" />
+        <span className="font-semibold mr-1">Session:</span>
+        <span>{values.session.label}</span>
+      </p>
+      <p>
+        <ClockIcon className="w-4 h-4 inline-block mr-2" />
+        <span className="font-semibold mr-1">Duration:</span>
+        <span>{values.duration} minutes</span>
+      </p>
+      <p>
+        <FolderIcon className="w-4 h-4 inline-block mr-2" />
+        <span className="font-semibold mr-1">Project:</span>
+        <span>{values.project.label}</span>
+      </p>
+      <p>
+        <BarChartIcon className="w-4 h-4 inline-block mr-2" />
+        <span className="font-semibold mr-1">Progress:</span>
+        <span>
+          {values.progress.emoji} {values.progress.label}
+        </span>
+      </p>
+      <p>
+        <PlaneIcon className="w-4 h-4 inline-block mr-2" />
+        <span className="font-semibold mr-1">Submission Type:</span>
+        <span>{values.submissionType}</span>
+      </p>
+      <p>
+        <MailboxIcon className="w-4 h-4 inline-block mr-2" />
+        <span className="font-semibold mr-1">Follow-up:</span>
+        <span>{values.follow_up}</span>
+      </p>
+      {values.personalNotes && (
+        <p className="col-span-2">
+          <FileIcon className="w-4 h-4 inline-block mr-2" />
+          <span className="font-semibold mr-1">Personal Notes:</span>
+          <br />
+          <span>{values.personalNotes}</span>
+        </p>
+      )}
+      <p className="col-span-2">
+        <LaptopIcon className="w-4 h-4 inline-block mr-2" />
+        <span className="font-semibold mr-1">Summary:</span>
+        <br />
+        <span>{values.summary}</span>
+      </p>
     </div>
   );
 };

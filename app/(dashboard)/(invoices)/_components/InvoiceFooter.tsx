527ccaf14ee89a1cba3bc8c726f77cfc0dc8c785
Simen Daehlin <simen.daehlin@strapi.io>
2023-12-28 08:58:50 +0000

    Add PDF export button and reorganize buttons in InvoiceFooter component

--- app/(dashboard)/(invoices)/_components/InvoiceFooter.tsx
+++ app/(dashboard)/(invoices)/_components/InvoiceFooter.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@nextui-org/react';
 import { Link } from '@nextui-org/react';
 import { Invoice } from '@prisma/client';
+import { FaFilePdf } from 'react-icons/fa';
 import { IoEyeSharp } from 'react-icons/io5';
 import { MdOutlineSend } from 'react-icons/md';
 interface InvoiceFooterProps {
@@ -10,16 +11,11 @@ interface InvoiceFooterProps {
 const InvoiceFooter = ({ invoice }: InvoiceFooterProps) => {
   return (
     <div className="flex justify-end space-x-2 p-6">
-      {!invoice.isPaid && (
-        <Button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300"
-          endContent={<MdOutlineSend />}
-          size="md"
-        >
-          Send Invoice
-        </Button>
-      )}
-      <Button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded transition duration-300">
+      <Button
+        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded transition duration-300"
+        endContent={<FaFilePdf />}
+        size="md"
+      >
         Export to PDF
       </Button>
       <Button
@@ -27,10 +23,20 @@ const InvoiceFooter = ({ invoice }: InvoiceFooterProps) => {
         className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded transition duration-300"
         endContent={<IoEyeSharp />}
         href={`/view/invoice/${invoice.id}?code=${invoice.code}`}
+        size="md"
         target="_blank"
       >
         View Public Invoice
       </Button>
+      {!invoice.isPaid && (
+        <Button
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300"
+          endContent={<MdOutlineSend />}
+          size="md"
+        >
+          Send Invoice
+        </Button>
+      )}
     </div>
   );
 };
